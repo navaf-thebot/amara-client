bf871fdb9a1baacf148d73c97229e4874b4846c5
navaf-thebot <navaf@thebot.agency>
2025-06-11 20:03:38 +0530

    refactor: remove Navbar and Footer components from RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '../components/themes/theme-provider'
-import Navbar from '@/components/navbar/Navbar'
-import Footer from '@/components/Footer'
 import LayoutWrapper from '@/components/LayoutWrapper'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -27,9 +25,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
         <LayoutWrapper>{children}</LayoutWrapper>
-          <Footer />
         </ThemeProvider>
       </body>
     </html>
