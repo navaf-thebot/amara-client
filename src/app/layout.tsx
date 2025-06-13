53c2482b82f45b8a1955510903de5fcd85ed405f
navaf-thebot <navaf@thebot.agency>
2025-06-13 19:36:49 +0530

    fix: Correct spelling of 'Amaraa Global' in metadata for consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ import LayoutWrapper from '@/components/LayoutWrapper'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Amara Global',
-  description: 'Amara Global',
+  title: 'Amaraa Global',
+  description: 'Amaraa Global',
    icons: {
     icon: '/favicon.ico',
   },
