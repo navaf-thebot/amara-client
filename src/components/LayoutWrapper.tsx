208c8f053b527670f40da1ec3c8e65f0007efe0f
navaf-thebot <navaf@thebot.agency>
2025-06-17 18:41:50 +0530

    refactor: simplify LayoutWrapper component by removing unnecessary state and effect hooks

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -2,24 +2,16 @@
 import { usePathname } from 'next/navigation';
 import Navbar from './navbar/Navbar';
 import Footer from './Footer';
-import { useEffect, useState } from 'react';
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const [hasMounted, setHasMounted] = useState(false);
-
-  useEffect(() => {
-    setHasMounted(true);
-  }, []);
-
+  
   const isHome = pathname === '/';
 
   return (
     <>
       <Navbar />
-      <div className={!hasMounted || isHome ? '' : 'pt-16'}>
-        {hasMounted ? children : null}
-      </div>
+      <div className={isHome ? '' : 'pt-16'}>{children}</div>
       <Footer />
     </>
   );
